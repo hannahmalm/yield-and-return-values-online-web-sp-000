4a6899fda36a01cf740b7ed114f63e560c09e730
Hannah Malm <hannahemalm616@gmail.com>
2020-11-30 23:42:42 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,7 +3,6 @@ require 'pry'
 def hello(array)
   i = 0
   while i < array.length
-  binding.pry
     yield(array[i])
     i += 1
   end
@@ -11,5 +10,4 @@ end
 
 #the return value of a while loop is always nil. If you want the method to return something else, you have to tell it to do something
 
-binding.pry
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
