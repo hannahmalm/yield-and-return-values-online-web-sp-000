599826c42470be2e42ee0fd610cb65fb435d8455
Hannah Malm <hannahemalm616@gmail.com>
2020-11-30 23:40:51 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -11,4 +11,4 @@ end
 #the return value of a while loop is always nil. If you want the method to return something else, you have to tell it to do something
 
 binding.pry
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
